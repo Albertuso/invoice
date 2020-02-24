4a0df01ae30db19ed05bc60b2bc71a4f16ce70eb
maxikiruki <jaimenavarrol97@gmail.com>
2020-02-24 16:51:22 +0100

    subido

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -15,26 +15,70 @@ use App\Entity\ProductLine;
 use App\Entity\SocialNetwork;
 use App\Entity\Supervisor;
 use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Security\LoginAuthenticator;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class MainController extends AbstractController{
     /**
      * @Route("/", name="main")
      */
-    public function index(){
+    public function index(HttpFoundationRequest $request, UserPasswordEncoderInterface $passwordEncoder): Response{
+
+        //Vista de Bienvenida
+            //Si pulsamos registrarnos
+            $user = new User();
+            $registrationForm=$this->createForm(RegistrationFormType::class, $user);
+            $registrationForm->handleRequest($request);
+            if($registrationForm->isSubmitted() && $registrationForm->isValid()){
+                $em= $this->getDoctrine()->getManager();
+                $user->setRoles(['ROLE_USER']);
+                $user->setPassword(
+                    $passwordEncoder->encodePassword(
+                        $user,
+                        $registrationForm->get('plainPassword')->getData()
+                    )
+                );
+                $em->persist($registrationForm);
+                $em->flush();
+                $this->addFlash('exito','Usuario registrado');
+                return $this->redirectToRoute('main');
+            }
+    
+            //Si pulsamos login
+            
+            $loginForm=$this->createForm(LoginAuthenticator::class, $request );
+            $loginForm->handleRequest($request);
+            if($loginForm->isSubmitted() && $loginForm->isValid()){
+            
+                return $this->redirectToRoute('main');
+            }
+    
+            return $this->render('main/index.html.twig', [
+                'controller_name' => 'PrincipalController',
+                'registrationForm'=>$registrationForm->createView(),
+                'loginForm'=>$loginForm->createView()
+            ]);
+
+        //Vista fin de bienvenida
+
         $repositoryUsers = $this->getDoctrine()->getRepository(User::class);
         $users = $repositoryUsers->findAll();
 
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
             'user' => $this->getUser(),
-            'users' => $users
+            'users' => $users,
         ]);
         //vista principal donde apareceran las empresas del usuario
-    }
+        
+        }
     /**
      * @Route("/edit", name="edit")
      */
-    public function edit(){
+        public function edit(){
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
