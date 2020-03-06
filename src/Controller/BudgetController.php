d0cd2ed81cdab26dfd7bc790f0b8883654923338
maxikiruki <jaimenavarrol97@gmail.com>
2020-03-06 02:46:07 +0100

    presupuesto form ok

--- src/Controller/BudgetController.php
+++ src/Controller/BudgetController.php
@@ -8,6 +8,7 @@ use App\Entity\Client;
 use App\Entity\Enterprise;
 use App\Entity\Product;
 use App\Entity\ProductLine;
+use App\Form\BudgetType;
 use App\Form\InvoiceType;
 use App\Repository\EnterpriseRepository;
 use App\Repository\InvoiceRepository;
@@ -40,10 +41,86 @@ class BudgetController extends AbstractController
         // $repositoryClient = $this->getDoctrine()->getRepository(Client::class);
         // $client = $repositoryClient->findOneById($idclient);
         return $this->render('budget/index.html.twig', [
-            'invoices' => $budgetRepository->findbyClient($idclient),
+            'budgets' => $budgetRepository->findbyClient($idclient),
             'idclient' => $idclient,
             'enterprise' => $enterprise,
             'enterprises' => $repositoryEnterprises->findAll(),
         ]);
     }
+  /**
+     * @Route("/new/client/{idclient}", name="budget_new", methods={"GET","POST"})
+     */
+    public function new(Request $request, $idclient): Response
+    {
+
+        // Cargo el cliente (por route)
+        $repositoryClient = $this->getDoctrine()->getRepository(Client::class);
+        $client = $repositoryClient->findOneById($idclient);
+
+        // Cargo la empresa a la que pertecene el cliente
+        $enterprise = $client->getEnterprise();
+
+        //Cargo los productos de esa empresa
+        $repositoryProduct = $this->getDoctrine()->getRepository(Product::class);
+        $products = $repositoryProduct->findByEnterpriseId($enterprise);
+
+        //Genero una nueva factura
+        $budget = new Budget();
+
+        // Pongo los datos por defecto        
+        $hoy = new \DateTime();
+        // $budget->setInvoicenumber($enterprise->getNextinvoicenumber());
+        $budget->setDate($hoy);
+        // $invoice->setVisible(true);
+        $budget->setFooter($enterprise->getFooter());
+
+        // Creo un array de Productlines
+        $this->ProductLine = new ArrayCollection();
+
+        // Se genera el formulario con los datos
+        $form = $this->createForm(BudgetType::class, $budget);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $budget->setClient($client);
+            $budget->setEnterprise($enterprise);
+            // $budget->setVisible(true);
+            $names = $_REQUEST['productName'];
+            $quantities = $_REQUEST['quantity'];
+            $prices = $_REQUEST['price'];
+            $vats = $_REQUEST['VAT'];
+
+            //Aqui toca comprobar que el presupuesto es valido
+
+            //Guardo las lineas 
+            for ($i = 0; $i < count($names); $i++) {
+
+                $newLine = new ProductLine();
+
+                $newLine->setName($names[$i]);
+                $newLine->setQuantity($quantities[$i]);
+                $newLine->setPrice($prices[$i]);
+                $newLine->setVat($vats[$i]);
+
+                $budget->addLine($newLine);
+            }
+
+            $entityManager->persist($budget);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('budget_index', ['idclient' => $idclient]);
+        }
+
+        return $this->render('budget/new.html.twig', [
+            'budget' => $budget,
+            'form' => $form->createView(),
+            'enterprise' => $enterprise,
+            'client' => $client,
+            'products' => $products,
+        ]);
+
+        
+    }
+
 }
